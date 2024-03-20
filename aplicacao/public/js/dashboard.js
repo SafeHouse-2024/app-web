5662c38074112eebc8946d622e0137b147616d0a
maarcos-floriano <marcos.floriano@sptech.school>
2024-03-20 17:34:06 -0300

    adicionei informações no sessionStorage para exemplo

--- aplicacao/public/js/dashboard.js
+++ aplicacao/public/js/dashboard.js
@@ -1,6 +1,8 @@
 function exibirComputadores(){
   //consultaBanco("/conexao/usuario/idUsuario=1", "GET"); quero pegar a resposta e exibir
   consultaBanco("/conexao/computador/idMaquina>0", "GET").then(function (computadores) {
+    console.log(computadores)
+    sessionStorage.IDMAQUINA = computadores[0].idMaquina;
     var lista = document.getElementById("listaComputadores");
     lista.innerHTML = "";
     computadores.forEach(function (computador) {
