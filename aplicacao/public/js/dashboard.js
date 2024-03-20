29f57354fda97b3bfa8bbfbb7aa21f65228c1201
maarcos-floriano <marcos.floriano@sptech.school>
2024-03-20 17:16:56 -0300

    apaguei a function antiga e deixei a nova funcionando na dash

--- aplicacao/public/js/dashboard.js
+++ aplicacao/public/js/dashboard.js
@@ -1,27 +1,18 @@
-function listarComputadores() {
-  return fetch("/computador/listar", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then(function (resposta) {
-      if (resposta.ok){
-        console.log(`#SUCESSO: ${resposta}`);
-        return resposta.json();
-      }
-    })
-    .catch(function (resposta) {
-      console.log(`#ERRO: ${resposta}`);
-    });
-}
-
 function exibirComputadores(){
-  consultaBanco("/conexao/usuario/idUsuario=1", "GET");
+  //consultaBanco("/conexao/usuario/idUsuario=1", "GET"); quero pegar a resposta e exibir
+  consultaBanco("/conexao/computador/idMaquina>0", "GET").then(function (computadores) {
+    var lista = document.getElementById("listaComputadores");
+    lista.innerHTML = "";
+    computadores.forEach(function (computador) {
+      var li = document.createElement("li");
+      li.appendChild(document.createTextNode(computador.idMaquina + " - " + computador.nomeMaquina + " - " + computador.processador + " - " + computador.memoriaRam + " - " + computador.discoRigido));
+      lista.appendChild(li);
+    });
+  });
 }
 
 function consultaBanco(caminho, metodo) {
-  fetch(`${caminho}`, {
+  return fetch(`${caminho}`, {
     method: `${metodo}`,
     headers: {
       "Content-Type": "application/json",
@@ -32,9 +23,6 @@ function consultaBanco(caminho, metodo) {
         console.log(`#SUCESSO: ${resposta}`);
         return resposta.json();
       }
-    })
-    .then(function (usuarios) {
-      console.log(usuarios);
     })
     .catch(function (resposta) {
       console.log(`#ERRO: ${resposta}`);
