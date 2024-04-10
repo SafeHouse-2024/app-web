0c2cf336e3da536c187ffcded8481c9b4a7063cf
maarcos-floriano <marcos.floriano@sptech.school>
2024-04-10 17:45:37 -0300

    criei uma composicao para cada maquina, separando header e graficos

--- aplicacao/public/js/dashboard.js
+++ aplicacao/public/js/dashboard.js
@@ -53,37 +53,38 @@ function trocarTela(tela) {
     }
   }
 }
-// google.charts.load('current', { 'packages': ['corechart'] });
-// google.charts.setOnLoadCallback(drawChart);
-// function drawChart() {
-//   // Create the data table.
-//   var data = new google.visualization.DataTable();
-//   data.addColumn('string', 'Topping');
-//   data.addColumn('number', 'Slices');
-//   data.addRows([
-//     ['Mushrooms', 3],
-//     ['Onions', 1],
-//     ['Olives', 1],
-//     ['Zucchini', 1],
-//     ['Pepperoni', 2]
-//   ]);
 
-//   // Set chart options
-//   var options = {
-//     'title': 'How Much Pizza I Ate Last Night',
-//     'width': 400,
-//     'height': 300
-//   };
+google.charts.load('current', { 'packages': ['corechart'] });
+google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+  // Create the data table.
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Topping');
+  data.addColumn('number', 'Slices');
+  data.addRows([
+    ['Mushrooms', 3],
+    ['Onions', 1],
+    ['Olives', 1],
+    ['Zucchini', 1],
+    ['Pepperoni', 2]
+  ]);
 
-//   // Instantiate and draw our chart, passing in some options.
-//   var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-//   chart.draw(data, options);
-// }
+  // Set chart options
+  var options = {
+    'title': 'How Much Pizza I Ate Last Night',
+    'width': 400,
+    'height': 300
+  };
+
+  // Instantiate and draw our chart, passing in some options.
+  var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+  chart.draw(data, options);
+}
 
 let maquinas = document.querySelectorAll('.lista-maquinas > li')
 
 maquinas.forEach(maquina => {
-  maquina.addEventListener('click', function () {
+  maquina.firstElementChild.addEventListener('click', function () {
     console.log(maquina);
     if(maquina.classList.contains('activeMaquina')){
       maquina.classList.remove('activeMaquina')
