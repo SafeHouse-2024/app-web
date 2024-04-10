a6c7741fc69d45abac76feb257016c4855b7c026
maarcos-floriano <marcos.floriano@sptech.school>
2024-04-10 17:28:33 -0300

    criei uma function para aumentar as maquinas com o click

--- aplicacao/public/js/dashboard.js
+++ aplicacao/public/js/dashboard.js
@@ -53,39 +53,42 @@ function trocarTela(tela) {
     }
   }
 }
-google.charts.load('current', { 'packages': ['corechart'] });
+// google.charts.load('current', { 'packages': ['corechart'] });
+// google.charts.setOnLoadCallback(drawChart);
+// function drawChart() {
+//   // Create the data table.
+//   var data = new google.visualization.DataTable();
+//   data.addColumn('string', 'Topping');
+//   data.addColumn('number', 'Slices');
+//   data.addRows([
+//     ['Mushrooms', 3],
+//     ['Onions', 1],
+//     ['Olives', 1],
+//     ['Zucchini', 1],
+//     ['Pepperoni', 2]
+//   ]);
 
-google.charts.setOnLoadCallback(drawChart);
+//   // Set chart options
+//   var options = {
+//     'title': 'How Much Pizza I Ate Last Night',
+//     'width': 400,
+//     'height': 300
+//   };
 
-function drawChart() {
-
-  // Create the data table.
-  var data = new google.visualization.DataTable();
-  data.addColumn('string', 'Topping');
-  data.addColumn('number', 'Slices');
-  data.addRows([
-    ['Mushrooms', 3],
-    ['Onions', 1],
-    ['Olives', 1],
-    ['Zucchini', 1],
-    ['Pepperoni', 2]
-  ]);
-
-  // Set chart options
-  var options = {
-    'title': 'How Much Pizza I Ate Last Night',
-    'width': 400,
-    'height': 300
-  };
-
-  // Instantiate and draw our chart, passing in some options.
-  var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-  chart.draw(data, options);
-}
+//   // Instantiate and draw our chart, passing in some options.
+//   var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+//   chart.draw(data, options);
+// }
 
 let maquinas = document.querySelectorAll('.lista-maquinas > li')
+
 maquinas.forEach(maquina => {
   maquina.addEventListener('click', function () {
-    console.log(maquina.innerText);
+    console.log(maquina);
+    if(maquina.classList.contains('activeMaquina')){
+      maquina.classList.remove('activeMaquina')
+    }else{
+      maquina.classList.add('activeMaquina')
+    }
   });
 });
