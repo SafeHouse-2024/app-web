0923232b258f9432a09b1e67acea842d632a5f36
Marcos Floriano <marcos.floriano@sptech.school>
2024-04-30 19:57:23 -0300

    criei um if para cada grafico

--- aplicacao/public/js/graficos.js
+++ aplicacao/public/js/graficos.js
@@ -5,24 +5,30 @@ const graficosDonut = document.querySelectorAll('.chart-donut > canvas');
 const barraProgresso = document.querySelectorAll('.header-maquina > progress');
 
 function sortearLabel() {
-    let valor = Math.floor(Math.random() * 100);
-    let data = [];
-    for (let i = 0; i < 5; i++) {
-        data.push(valor);
-        valor = Math.floor(Math.random() * 100);
-    }
+  let valor = Math.floor(Math.random() * 100);
+  let data = [];
+  for (let i = 0; i < 5; i++) {
+    data.push(valor);
+    valor = Math.floor(Math.random() * 100);
+  }
 
-    return data;
+  return data;
 }
 
+const decidirNome = (decidi) => {
+ return decidi%2 ? 'CPU' : 'RAM';
+}
+
+let contador = 1;
+
 graficosLinha.forEach((grafico) => {
-    const ctx = grafico.getContext('2d');
+  const ctx = grafico.getContext('2d');
     new Chart(ctx, {
         type: 'line',
         data: {
             labels: ['12:00', '12:30', '13:00', '13:30', '14:00'],
             datasets: [{
-                label: 'CPU',
+                label: decidirNome(contador++), 
                 data: sortearLabel(),
                 borderColor: '#ff0000',
                 backgroundColor: 'transparent',
@@ -33,25 +39,25 @@ graficosLinha.forEach((grafico) => {
 });
 
 graficosDonut.forEach((grafico) => {
-    const ctx = grafico.getContext('2d');
-    new Chart(ctx, {
-        type: 'doughnut',
-        data: {
-            labels: ['CPU', 'RAM', 'Disco'],
-            datasets: [{
-                data: [30, 40, 30],
-                backgroundColor: ['#ff0000', '#00ff00', '#0000ff']
-            }]
-        }
-    });
+  const ctx = grafico.getContext('2d');
+  new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+      labels: ['CPU', 'RAM', 'Disco'],
+      datasets: [{
+        data: [30, 40, 30],
+        backgroundColor: ['#ff0000', '#00ff00', '#0000ff']
+      }]
+    }
+  });
 });
 
 function sortearValor() {
-    return Math.floor(Math.random() * 100);
+  return Math.floor(Math.random() * 100);
 }
 
 setInterval(() => {
   barraProgresso.forEach((barra) => {
     barra.value = sortearValor();
-});
+  });
 }, 2000);
