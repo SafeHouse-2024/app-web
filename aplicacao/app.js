cc46d1e1d47b59f46b8e086d61f6dcd56c1abce3
Ian Silva Santos <ian.santos@sptech.school>
2024-05-12 21:38:08 -0300

    Adicionando a configuração do websocket server e suas respectivas dependências

--- aplicacao/app.js
+++ aplicacao/app.js
@@ -3,7 +3,13 @@ const app = express();
 const porta = 3000;
 var cors = require("cors");
 var path = require("path");
-
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*",
+        method: ["GET", "POST"]
+    }
+});
 
 app.use(cors());
 
@@ -18,6 +24,21 @@ var rotaSlack = require("./src/routers/slack");
 app.use("/", rotaIndex);
 app.use("/conexao", rotaConexao);
 app.use("/slack", rotaSlack);
+
+io.listen(3001);
+
+io.on('connection', (socket) => {
+    //if(socket.handshake.headers.token != '123') return 
+    let nomeEmpresa = socket.handshake.headers.empresa;
+    let macAddress = "a4:63:a1:6d:0f:f7"
+    console.log(socket.handshake.headers);
+    socket.on(`send_message_${nomeEmpresa}`, (data) => {
+        socket.broadcast.emit(`receive_message_${nomeEmpresa}`, data)
+    })
+    socket.on(`send_message_${macAddress}`, (data) => {
+        socket.broadcast.emit(`receive_message_${macAddress}`, data)
+    })
+})
 
 app.listen(porta, () => {
     console.log(`Aplicação rodando nesse caminho http://localhost:${porta}`);
